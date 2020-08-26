e0f023ec665af713c4a738db7c21549020a828f1
Denys Batrak <baden.i.ua@gmail.com>
2020-08-26 16:41:22 +0300

    Updates

---
+++ precache-manifest.15ad91cb8e6d329e67966657adeeb594.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "a2722378a7d3ce7f116b5f35c050b206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b61e9f75fef223baf1228f3163d2a8ba",
+    "url": "/product01.jpg"
+  },
+  {
+    "revision": "c49db6238635d0c87f18b142038e0562",
+    "url": "/sony.mp4"
+  },
+  {
+    "revision": "60558c200da90cde057a",
+    "url": "/static/css/main.f8751acd.chunk.css"
+  },
+  {
+    "revision": "60558c200da90cde057a",
+    "url": "/static/js/main.60558c20.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "/static/js/vendors~main.604c6712.chunk.js"
+  }
+]);
